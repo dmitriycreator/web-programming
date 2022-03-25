cbbadb3662540ac1a0f7f36dbc2ec797dd318e87
dmitriycreator <zar1970korol@gmail.com>
2022-03-25 22:11:36 +0300

    html page commit

---
+++ lesson_2/form.js
@@ -0,0 +1,25 @@
+function validate()
+{
+    let x = document.form.name.value;
+    let y = document.form.pass.value;
+    let check = true;
+    if (x.length === 0) {
+        document.getElementById("login").style.backgroundColor = '#CD5C5CFF';
+        document.getElementById("login-text").style.color = 'red';
+        document.getElementById("login-text").innerText = '*this field must be filled';
+        check = false;
+    }
+    if (y.length === 0) {
+        document.getElementById("password").style.backgroundColor = '#CD5C5CFF';
+        document.getElementById("password-text").style.color = 'red';
+        document.getElementById("password-text").innerText = '*this field must be filled';
+        check = false;
+    }
+    else if (y.length <=3) {
+        document.getElementById("password").style.backgroundColor = '#CD5C5CFF';
+        document.getElementById("password-text").style.color = 'red';
+        document.getElementById("password-text").innerText = '*password cannot be less than 4 symbols';
+        check = false;
+    }
+    return check;
+}
