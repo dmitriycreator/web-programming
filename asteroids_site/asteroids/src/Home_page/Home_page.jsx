322da3d11581ac3d3ecdda28d39fbf117abf9e17
dmitriycreator <zar1970korol@gmail.com>
2022-05-21 10:01:53 +0300

    Dynamic asteroids list added

--- asteroids_site/asteroids/src/Home_page/Home_page.jsx
+++ asteroids_site/asteroids/src/Home_page/Home_page.jsx
@@ -4,11 +4,47 @@ import { AsteroidCard } from "../Asteroid_card/AsteroidCard"
 import ARMAGEDDON from "./ARMAGGEDON_V.svg"
 
 export const Home_page = () => {
+
+    const Make_Asteroids_List = () =>{
+        let monthNames=[
+            'января',
+            'февраля',
+            'марта',
+            'апреля',
+            'мая',
+            'июня',
+            'июля',
+            'августа',
+            'сентября',
+            'ноября',
+            'декабря',
+        ]
+        let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+        let list = []
+        for(let i = 0; i < 10; i++){
+            let year = "20" + (Math.random()*89 + 10).toFixed(0)
+            let name = year + ' ' + characters[(Math.random()*25).toFixed(0)] + characters[(Math.random()*25).toFixed(0)]
+            let date = (Math.random()*27 + 1).toFixed(0) + ' ' + monthNames[(Math.random()*12).toFixed(0)] + ' ' + year + " года"
+            let grade = Math.random() < 0.5? "опасен":"не опасен";
+            let size = (Math.random() * 100 + 10).toFixed(0)
+            let distance = (Math.random() * 9000000 + 1000000).toFixed(0)
+            list.push({
+                name:name,
+                date:date,
+                grade:grade,
+                size:size,
+                distance:distance
+            })
+        }
+        return list;
+    }
+
     //asteroids list
     const [asteroids, setAsteroids] = useState({
-        list:[{name: "2021 FQ", date: "12 сентября 2021", grade: "не опасен", distance: 7235024, size: 85},
-            {name: "2021 ER", date: "2 ноября 2021", grade: "не опасен", distance: 9331775, size: 300},
-            {name: "2022 QQ", date: "3 матра 2022", grade: "опасен", distance: 2866012, size: 850}],
+        list: Make_Asteroids_List(),
+        distanceMode:0,
+        showMode:0
     });
 
     const changeShowMode = (e) => {
