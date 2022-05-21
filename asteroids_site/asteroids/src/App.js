8c57bbf8f73d9cd5cca0ca0c28c31e17a58c7a9a
dmitriycreator <zar1970korol@gmail.com>
2022-05-21 18:08:09 +0300

    Added Login Form

--- asteroids_site/asteroids/src/App.js
+++ asteroids_site/asteroids/src/App.js
@@ -1,58 +1,65 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Switch } from 'react-router-dom';
-import { Home } from './Home_page/Home_page.jsx';
-import { ASTEROID_DISTANCE_MODE_KM, ASTEROID_SHOW_MODE_ALL } from './Constants/Constants_list';
-import { reducer } from './Reducer';
-import React, { createContext, useReducer, useEffect } from 'react';
-import { DestructionPage } from './Destruction/Destruction_page';
-import { convertAPIDataToList, getAPIUrl } from './Home_page/Parser';
-// import './App.css';
+import "./App.css";
+import {Route, Routes} from "react-router-dom";
+import {Home} from "./Home/Home";
+import {AsteroidCard} from "./AsteroidCard/AsteroidCard";
+import {createContext, useReducer, useEffect} from "react";
+import {reducer} from "./Reducer";
+import {
+    ASTEROID_DISTANCE_MODE_KM, ASTEROID_SHOW_MODE_ALL,
+} from "./AsteroidConstants/AsteroidConstants";
+import {DestructionPage} from "./DestructionPage/DestructionPage";
+import {
+    ConvertAPIDataToList, GetAPIUrl, GetDefaultApiKey
+} from "./ApiUtils";
+import {LoginPage} from "./LoginPage/LoginPage";
 
 export const AsteroidsContext = createContext(null);
 
-/**
- * class App
- */
-class App extends React.Component {
-  /**
-   *
-   * @return {JSX.Element}
-   */
-  render() {
+function App() {
     const [state, dispatch] = useReducer(reducer, {
-      asteroidsList: [],
-      destructionList: [],
-      distanceMode: ASTEROID_DISTANCE_MODE_KM,
-      showMode: ASTEROID_SHOW_MODE_ALL
+        asteroidsList: [],
+        destructionList: [],
+        distanceMode: ASTEROID_DISTANCE_MODE_KM,
+        showMode: ASTEROID_SHOW_MODE_ALL,
+        userProfile: {
+            name: "", apiKey: GetDefaultApiKey(),
+        },
     });
 
     useEffect(() => {
-      fetch(getAPIUrl())
-        .then((response) =>
-          response.json().then((resData) => {
-            dispatch({ payload: convertAPIDataToList(resData), type: 'UPDATE_ASTEROIDS_LIST' });
-          })
-        )
-        .catch((error) => console.log(error));
+        if (window.localStorage.getItem("customApiKey")) {
+            dispatch({
+                payload: {
+                    name: window.localStorage.getItem("username"), apiKey: window.localStorage.getItem("customApiKey"),
+                }, type: "SET_USER_PROFILE",
+            });
+        }
+
+        fetch(GetAPIUrl(state.userProfile.apiKey))
+            .then((response) => response.json().then((resData) => {
+                dispatch({
+                    payload: ConvertAPIDataToList(resData), type: "UPDATE_ASTEROIDS_LIST",
+                });
+            }))
+            .catch((error) => console.log(error));
     }, []);
 
-    return (
-      <Router>
-        <div>
-          <AsteroidsContext.Provider value={{ state: state, dispatch: dispatch }}>
-            <Switch>
-              <Route path={'/'}>
-                <Home />
-              </Route>
-              <Route path={'/Destroy'}>
-                <DestructionPage />
-              </Route>
-            </Switch>
-          </AsteroidsContext.Provider>
-        </div>
-      </Router>
-    );
-  }
+    return (<div>
+            <AsteroidsContext.Provider
+                value={{state: state, dispatch: dispatch}}
+            >
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="toDestroy" element={<DestructionPage/>}/>
+                    <Route
+                        path="login"
+                        element={<center>
+                            <LoginPage/>
+                        </center>}
+                    />
+                </Routes>
+            </AsteroidsContext.Provider>
+        </div>);
 }
 
 export default App;
